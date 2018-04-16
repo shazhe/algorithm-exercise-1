6a028dec1e68b311e0471cacf7ca3a385cc59c55
Yuting Wen <yutingyw@gmail.com>
2018-04-16 19:16:14 +1000

    minor changes

--- algorithms_medium.py
+++ algorithms_medium.py
@@ -614,6 +614,37 @@ def balance_parentheses(lst):
 def test_bp():
     assert balance_parentheses(')((())') == '(())'
     assert balance_parentheses(')))(()))())') == '(())()'
+
+"""
+Check whether or not an input string's openers and closers are properly nested.
+"""
+def is_valid(code):
+    openers_to_closers = {
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
+    }
+
+    openers = frozenset(openers_to_closers.keys())
+    closers = frozenset(openers_to_closers.values())
+
+    openers_stack = []
+
+    for char in code:
+        if char in openers:
+            openers_stack.append(char)
+        elif char in closers:
+            if not openers_stack:
+                return False
+            else:
+                last_unclosed_opener = openers_stack.pop()
+
+                # if this closer doesn't correspond to the most recently
+                # seen unclosed opener, short-circuit, returning false
+                if not openers_to_closers[last_unclosed_opener] == char:
+                    return False
+
+    return openers_stack == []
     
 """
 takes stock_prices_yesterday and returns the best profit
